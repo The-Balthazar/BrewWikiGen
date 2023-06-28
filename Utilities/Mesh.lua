12eca4a322e44932fdb90031d335ff4902cd072a
Balthazar <the.balthazar@Gmail.com>
2023-06-28 20:30:45 +0100

    \00 to \000 binary values.

--- Utilities/Mesh.lua
+++ Utilities/Mesh.lua
@@ -15,14 +15,14 @@ local function FindSCMBoneArrayStartTagOffset(filestring)
 end
 
 local function FindSCMBoneArrayEndTagOffset(filestring, startoffset)
-    return string.find(filestring, 'SKEL\00\00\80\63', startoffset+16) --Catches ~99%
+    return string.find(filestring, 'SKEL\000\000\080\063', startoffset+16) --Catches ~99%
     or string.find(filestring, '\197SKEL', startoffset+15) --Catches 15/16, but only vanilla or 3dsmax exported. Blender uses the print-able character X as padding instead.
-    or string.find(filestring, '\00SKEL', startoffset+15) + 1 --Catches the remaining 1/16, but has overlap.
+    or string.find(filestring, '\000SKEL', startoffset+15) + 1 --Catches the remaining 1/16, but has overlap.
 end
 
 local function GetArrayFrom0x00DelimitedString(str)
     local things = {}
-    string.gsub(str, '([^\00]+)\00', function(thing)
+    string.gsub(str, '([^\000]+)\000', function(thing)
         table.insert(things, thing)
     end)
     return things
